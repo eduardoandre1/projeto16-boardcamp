06f19cf67fbbf01d366c83b9f60ccce199ea0a8e
Eduardo André <eduardo_andre@id.uff.br>
2023-07-27 19:36:35 -0300

    reactor:

--- src/database/postgresSQL.js
+++ src/database/postgresSQL.js
@@ -3,13 +3,7 @@ import dotenv from "dotenv";
 dotenv.config()
 const {Pool} = pg;
 const DB = new Pool({
-    //user: 'bootcamp_role',
-    //host:'localhost',
-    //database:'boardcamp',
-    //password:'eduardo',
-    //port:5432,
-    connectionString:'postgres://bootcamp_role:eduardo@localhost:5432/boardcamp' || process.env.DATABASE_URL
-
+    connectionString:process.env.DATABASE_URL || 'postgres://bootcamp_role:eduardo@localhost:5432/boardcamp'
 })
-const server_status = DB.connect()
+
 export default DB
