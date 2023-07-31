b3ebc51717c3abac0c6af86295dd1cf6dd38ba19
eduardo andre <eduardo_andre@id.uff.br>
2023-07-31 12:09:39 -0300

    feat: ordened list games and customers

--- src/controlers/customers/customers_list.js
+++ src/controlers/customers/customers_list.js
@@ -2,7 +2,7 @@ import DB from "../../database/postgresSQL.js"
 
 async function customers_list(req,res){ 
     try{
-        const table = await DB.query(`SELECT id,name,phone,cpf,TO_CHAR(customers.birthday, 'YYYY-MM-DD') as birthday FROM customers;`)
+        const table = await DB.query(`SELECT id,name,phone,cpf,TO_CHAR(customers.birthday, 'YYYY-MM-DD') as birthday FROM customers ORDER BY id;`)
         return res.status(200).send(table.rows)
     }catch(err){return res.status(500).send(err.message)}
 }
--- src/controlers/games/games_list.js
+++ src/controlers/games/games_list.js
@@ -1,7 +1,7 @@
 import DB from "../../database/postgresSQL.js"
 async function gamers_list(req,res){ 
     try{
-        const table = await DB.query('SELECT * FROM games;')
+        const table = await DB.query('SELECT * FROM games ORDER BY id;')
         return res.status(200).send(table.rows)
     }catch(err){return res.status(500).send(err.message)}
     
