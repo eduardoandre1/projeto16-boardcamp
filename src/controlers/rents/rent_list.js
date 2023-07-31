5572674b4224abbda76e74a4682573ff34e35091
eduardo andre <eduardo_andre@id.uff.br>
2023-07-31 12:20:22 -0300

    feat: ordened rent_list

--- src/controlers/rents/rent_list.js
+++ src/controlers/rents/rent_list.js
@@ -5,7 +5,8 @@ async function rent_list(req,res){
     try{
         const table = await DB.query(rent_list_join)
         const table_formated = table.rows.map((object)=>rent_list_formater(object))
-        return res.status(200).send(table_formated)
+        const table_formated_ordened = table_formated.sort((a,b)=>{return a.id-b.id})
+        return res.status(200).send(table_formated_ordened)
     }catch(err){return res.status(500).send(err.message)}
 }
 export default rent_list
